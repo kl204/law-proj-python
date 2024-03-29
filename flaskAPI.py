30e36ff45ba62586d65be1db01cf22d9de4d306c
PUO <86181292+kl204@users.noreply.github.com>
2024-03-29 21:00:53 +0900

    post test

--- flaskAPI.py
+++ flaskAPI.py
@@ -91,41 +91,43 @@ def go_chat():
     print(thread_id)
     print(message_content)
 
-    message = client.beta.threads.messages.create(
+    # message = client.beta.threads.messages.create(
 
-        thread_id,
-        role="user",
-        content=message_content
-    )
+    #     thread_id,
+    #     role="user",
+    #     content=message_content
+    # )
 
  
-    run = client.beta.threads.runs.create(
-    thread_id=thread_id,
-    assistant_id=my_assistant_id
-    )
+    # run = client.beta.threads.runs.create(
+    # thread_id=thread_id,
+    # assistant_id=my_assistant_id
+    # )
 
 
-    run_id = run.id
+    # run_id = run.id
 
 
-    while True:
-        run = client.beta.threads.runs.retrieve(
-        thread_id=thread_id,
-        run_id=run_id
-        )
-        if run.status == "completed":
-            break
-        else:
-            print("로딩중..")
-            time.sleep(2)
+    # while True:
+    #     run = client.beta.threads.runs.retrieve(
+    #     thread_id=thread_id,
+    #     run_id=run_id
+    #     )
+    #     if run.status == "completed":
+    #         break
+    #     else:
+    #         print("로딩중..")
+    #         time.sleep(2)
 
 
-    thread_messages = client.beta.threads.messages.list(thread_id)
-    print(thread_messages.data[0].content[0].text.value)
+    # thread_messages = client.beta.threads.messages.list(thread_id)
+    # print(thread_messages.data[0].content[0].text.value)
 
-    answer_chatgpt = thread_messages.data[0].content[0].text.value
+    # answer_chatgpt = thread_messages.data[0].content[0].text.value
 
-    return answer_chatgpt
+    # return answer_chatgpt
+
+    return '메세지 : ' + message_content
 
            
 if __name__ == '__main__':
