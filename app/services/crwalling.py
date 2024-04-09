a8cbcfb55a8ddc23fc9ae505f18f83ef51166a84
PUO <86181292+kl204@users.noreply.github.com>
2024-04-09 11:31:31 +0900

    키워드 추가

--- app/services/crwalling.py
+++ app/services/crwalling.py
@@ -84,13 +84,13 @@ def crawling(licPrec, searchText):
                 h5_text = h5.text.replace(" ", "")
         
                 # "원고" 키워드가 포함된 경우
-                if "원고" in h5_text:
+                if "원고" "상 고 인" in h5_text:
                     p = h5.find_next('p', {'class': 'pty4_dep1'})
                     if p:
                         data["texts"].append({"category": "원고", "text": p.text})
 
                 # "피고" 키워드가 포함된 경우
-                elif "피고" in h5_text:
+                elif "피고" "피 고 인" in h5_text:
                     p = h5.find_next('p', {'class': 'pty4_dep1'})
                     if p:
                         data["texts"].append({"category": "피고", "text": p.text})
@@ -101,11 +101,17 @@ def crawling(licPrec, searchText):
                     if p:
                         data["texts"].append({"category": "주문", "text": p.text})
 
-                # "주문" 키워드가 포함된 경우
+                # "이유" 키워드가 포함된 경우
                 elif "이유" in h5_text:
                     p = h5.find_next('p', {'class': 'pty4_dep1'})
                     if p:
                         data["texts"].append({"category": "이유", "text": p.text})
+
+                # "원심판결" 키워드가 포함된 경우
+                elif "원심판결" in h5_text:
+                    p = h5.find_next('p', {'class': 'pty4_dep1'})
+                    if p:
+                        data["texts"].append({"category": "원심판결", "text": p.text})
 
         return data
 
